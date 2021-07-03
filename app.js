0349975a2bd82c8e3473fae63c8b2be758ce9c1b
David Kerr <david.kerr766@outlook.com>
2021-07-03 14:09:49 +1000

    connects to atlas

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const atlasUri = `mongodb+srv://express-server:${process.env.ATLAS_PASS}@cluster0.c0qpm.mongodb.net/aperture?retryWrites=true&w=majority`;
+mongoose.connect(atlasUri, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+  if (err) console.log(err);
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('connected to AtlasDB');
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World');
